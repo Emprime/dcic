e0a307a78a8b35e32a254d28ad700c52bd22851d
Emprime <Emprime@users.noreply.github.com>
2023-11-08 13:43:45 +0100

    Add lite setup to checkup

--- src/util/check_and_setup.py
+++ src/util/check_and_setup.py
@@ -1,10 +1,10 @@
 from absl import app
-from src.util.const import get_all_dataset_infos
+from src.util.const import get_all_dataset_infos, get_lite_dataset_infos
 import os
 
 def main(args):
 
-    for dataset_name, dataset_class in sorted(get_all_dataset_infos().items()):
+    for dataset_name, dataset_class in sorted(get_lite_dataset_infos().items()):
         print(f"Check & setup dataset {dataset_name}")
         # check naming is correct
         assert dataset_name == dataset_class.name
