3bc9cd01b16ffcb04e42efdb3c898e7655d8d0d6
Emprime <Emprime@users.noreply.github.com>
2023-11-08 15:33:27 +0100

    Tree smaller

--- src/datasets/treeversity6.py
+++ src/datasets/treeversity6.py
@@ -3,4 +3,4 @@ from src.datasets.common.dataset_skeleton import DatasetSkeleton
 
 class Treeversity6(DatasetSkeleton):
     def __init__(self, root_directory):
-        DatasetSkeleton.__init__(self,'Treeversity#6',224,['bark', 'bud', 'flower', 'fruit', 'leaf', 'whole_plant'],[1892, 1972, 1972, 2032, 1958], 0, [1011, 1839, 3072, 1170, 1872, 862],[0.4439581940620345, 0.4509297096690951, 0.3691211738638277], [0.23407518616927706, 0.22764417468550843, 0.2600833107790479], root_directory)
+        DatasetSkeleton.__init__(self,'Treeversity#6',112,['bark', 'bud', 'flower', 'fruit', 'leaf', 'whole_plant'],[1892, 1972, 1972, 2032, 1958], 0, [1011, 1839, 3072, 1170, 1872, 862],[0.4439581940620345, 0.4509297096690951, 0.3691211738638277], [0.23407518616927706, 0.22764417468550843, 0.2600833107790479], root_directory)
